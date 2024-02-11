682df306949af8f8464583152c44da183c6f0f1b
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-02-11 17:04:45 +0600

    Add on going functionalities and apply some style

--- app/(tabs)/OnGoing.js
+++ app/(tabs)/OnGoing.js
@@ -4,15 +4,31 @@ import { useSelector } from "react-redux";
 
 const OnGoing = () => {
   const onGoingWork = useSelector((state) => state.ToDoData.onGoingTodos);
-  console.log("first", onGoingTodos);
   return (
     <View>
       <Text>On Going Works</Text>
       <View>
-        {onGoingWork.map((todos, index) => {
-          <View key={index}>
-            <Text>{todos.todo}</Text>
-          </View>;
+        {onGoingWork?.map((todos, index) => {
+          return (
+            <View
+              key={index}
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginBottom: 20,
+                padding: 10,
+                borderWidth: 1,
+                borderRadius: 10,
+                borderColor: "black",
+                margin: 10,
+              }}
+            >
+              <Text>
+                {index + 1}. {todos.todo}
+              </Text>
+            </View>
+          );
         })}
       </View>
     </View>
